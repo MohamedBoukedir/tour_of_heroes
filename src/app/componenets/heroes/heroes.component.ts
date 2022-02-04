6a6fb151b6a33ff7472e9491303e81756bcd6270
bouukedir@outlook.com <Bouukedir@outlook.com>
2022-02-04 10:13:45 +0100

    add messsages components

--- src/app/componenets/heroes/heroes.component.ts
+++ src/app/componenets/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../Hero';
-import { HEROES } from '../mock-heroes';
-
+import { Hero } from '../../Hero';
+import { HeroService } from '../../hero.service';
+import { MessageService } from '../../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,13 +10,23 @@ import { HEROES } from '../mock-heroes';
 
 //Always export the component class so you can import it elsewhere ... like in the AppModule.
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
-  constructor() {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
 
   // Angular calls ngOnInit() shortly after creating a component. It's a good place to put initialization logic.
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
